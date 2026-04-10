7da43d56a7b3871ed6b6846cbca3e3ad0e25dad8
Ivan Petrov <ivanpetrov.main@gmail.com>
2026-04-10 03:44:29 -0600

    feat: add d00/ex01

---
+++ d00/ex01/ft_countdown.ml
@@ -0,0 +1,16 @@
+(* eval returns a function *)
+let rec ft_countdown =
+  fun n -> (* type will be int -> unit because type inference from print_int n *)
+      if n > 0 then (
+        print_int n; 
+        print_char '\n';
+        ft_countdown(n - 1))
+      else (
+        print_int 0; 
+        print_char '\n')
+
+(* we eval right to get matched to () *)
+let () =
+  ft_countdown 3;
+  ft_countdown 0;
+  ft_countdown (-1)
