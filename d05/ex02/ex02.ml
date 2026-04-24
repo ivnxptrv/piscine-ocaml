e1121652bfa13b3cf007afd89369c6ae75e7658b
Ivan Petrov <ivanpetrov.main@gmail.com>
2026-04-24 02:09:27 -0600

    feat: add d05/ex02

---
+++ d05/ex02/ex02.ml
@@ -0,0 +1,26 @@
+module type PAIR = sig
+  val pair : int * int
+end
+
+module type VAL = sig
+  val x : int
+end
+
+(* FIX ME !!! *)
+module Pair : PAIR = struct
+  let pair = (21, 42)
+end
+
+module MakeFst (Module : PAIR) = struct
+  let x, _ = Module.pair
+end
+
+module MakeSnd (Module : PAIR) = struct
+  let _, x = Module.pair
+end
+
+module Fst : VAL = MakeFst (Pair)
+
+module Snd : VAL = MakeSnd (Pair)
+
+let () = Printf.printf "Fst.x = %d, Snd.x = %d\n" Fst.x Snd.x
